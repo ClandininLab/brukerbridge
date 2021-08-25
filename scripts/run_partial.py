6129371863fec24d50172963bff3f7238c27edbf
lukebrez <brezovec@stanford.edu>
2021-08-25 16:51:36 -0700

    email

--- scripts/run_partial.py
+++ scripts/run_partial.py
@@ -4,17 +4,19 @@ extensions_for_oak_transfer = ['.nii', '.csv', '.xml', 'json', '.txt'] # last 4 
 
 ### Directory on this computer to process ###
 # full_target = 'G:/Max/20210611'
-# full_target = 'G:/Tim/20210427'
+# full_target = 'G:/Tim/20210824'
+full_target = 'G:/Michelle/210805'
 # full_target = 'G:/Avery/20210614'
-full_target = 'G:/Ashley/20210802'
+# full_target = 'G:/Ashley/20210601'
 #full_target = 'F:/Ashley/20210709_2'
 #full_target = 'X:/data/Ashley2/imaging_raw/20210709'
 
 ### Oak target ###
 # oak_target = 'X:/data/Brezovec/2P_Imaging/imports'
-oak_target = 'X:/data/Ashley2/imports'
+# oak_target = 'X:/data/Ashley2/imports'
 # oak_target = 'X:/data/Max/ImagingData/Bruker/imports'
-# oak_target = 'X:/data/Tim/ImagingData/imports'
+#oak_target = 'X:/data/Tim/ImagingData/imports'
+oak_target = 'X:/data/Michelle/Bruker/imports'
 # oak_target = 'X:/data/krave/bruker_data/imports/test'
 
 ### raw to tiff ###
@@ -22,8 +24,8 @@ bridge.convert_raw_to_tiff(full_target)
 
 ### tiffs to nii or tiff stack ###
 #bridge.convert_tiff_collections_to_nii(full_target)
-bridge.convert_tiff_collections_to_nii(full_target)
-#bridge.convert_tiff_collections_to_stack(full_target)
+#bridge.convert_tiff_collections_to_nii(full_target)
+bridge.convert_tiff_collections_to_stack(full_target)
 
 ### Transfer to oak ###
 bridge.start_oak_transfer(full_target, oak_target, extensions_for_oak_transfer, "False")
