823b9c41961f31a83c7c747cb287af497da6faf7
Andrew Berger <andbberger@gmail.com>
2024-08-02 00:07:04 -0700

    fixes some windows bullshit with the .bat

--- scripts/launch_bridge.bat
+++ scripts/launch_bridge.bat
@@ -1,3 +1,5 @@
 @echo off
 @echo watching for files
+:: I resent this but there is some sort of environment running it elsewhere and I cannot be asked
+cd /d "C:\Users\User\src\brukerbridge"
 py -3 -m brukerbridge
