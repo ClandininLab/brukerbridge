371893f4777d54ed63a754c0c3c55b4323606c3b
Andrew Berger <andbberger@gmail.com>
2024-08-01 23:28:54 -0700

    removes obsolete scripts

--- scripts/ripper.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Started ripper: %date% %time%
-set FOLDER_NAME=%1
-echo Ripping from folder %FOLDER_NAME%.
-"C:\Program Files\Prairie 5.5.64.600\Prairie View\Utilities\Image-Block Ripping Utility.exe" -isf -arfwsf %FOLDER_NAME% -cnv
-echo Finished ripping: %date% %time%
--- scripts/ripper_killer.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Killing ripper
-taskkill /IM "Image-Block Ripping Utility.exe"
