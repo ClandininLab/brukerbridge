116cf034745bbd8a03ffce0d31601ea86368a85c
Andrew Berger <andbberger@gmail.com>
2024-08-02 00:10:21 -0700

    tweaks .bat to suppress output

--- scripts/launch_bridge.bat
+++ scripts/launch_bridge.bat
@@ -2,4 +2,4 @@
 @echo watching for files
 :: I resent this but there is some sort of environment running it elsewhere and I cannot be asked
 cd /d "C:\Users\User\src\brukerbridge"
-py -3 -m brukerbridge
+py -3 -m brukerbridge >nul 2>&1
