d36cd4fbbb32db6a245c41c9f979fb604c1337d7
Andrew Berger <andbberger@gmail.com>
2024-08-12 19:08:05 -0600

    adds some more information to the .bat stdout

--- scripts/launch_bridge.bat
+++ scripts/launch_bridge.bat
@@ -1,5 +1,7 @@
 @echo off
 @echo watching for files
+@echo follow C:\Users\User\logs\bridge.log using mtail for info
+@echo logs from previous days are renamed
 :: I resent this but there is some sort of environment running it elsewhere and I cannot be asked
 cd /d "C:\Users\User\src\brukerbridge"
 py -3 -m brukerbridge >nul 2>&1
