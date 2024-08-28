cc2bd98c89be32a43e36777e1c2cb8b05ac1e928
Andrew Berger <andbberger@gmail.com>
2024-08-27 23:21:51 -0600

    fixes broken relative import

--- brukerbridge/legacy/__init__.py
+++ brukerbridge/legacy/__init__.py
@@ -1,3 +1,5 @@
-from tiff_to_nii import convert_tiff_collections_to_nii
-from tiff_to_nii_split import convert_tiff_collections_to_nii_split
-from tiffs_to_tiff_stack import convert_tiff_collections_to_stack
+from brukerbridge.legacy.tiff_to_nii import convert_tiff_collections_to_nii
+from brukerbridge.legacy.tiff_to_nii_split import \
+    convert_tiff_collections_to_nii_split
+from brukerbridge.legacy.tiffs_to_tiff_stack import \
+    convert_tiff_collections_to_stack
