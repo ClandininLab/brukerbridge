855f9d8f50d6c85ebdbb3a94e82f02043a015579
Andrew Berger <andbberger@gmail.com>
2024-08-30 23:59:34 -0600

    fixes dir level off by one in oak transfer
    previous poorly named code was labeled as sess relative when it was in fact user
    relative. now fixed to properly use a user relative target path, which will
    store TSeries within their respective sessions on oak

--- brukerbridge/transfer_to_oak.py
+++ brukerbridge/transfer_to_oak.py
@@ -73,8 +73,9 @@ def start_oak_transfer(
     # for some reason Path.parents is its own bizarre object that is not an
     # iterator and only supports positive indexing
     sess_path = list(acq_path.parents)[-3]
-    sess_relative_acq_path = acq_path.relative_to(sess_path)
-    target_path = oak_target / sess_relative_acq_path
+    user_path = sess_path.parent
+    user_relative_acq_path = acq_path.relative_to(user_path)
+    target_path = oak_target / user_relative_acq_path
     logger.debug("target path: %s", target_path)
 
     target_path.mkdir(parents=True, exist_ok=True)
