808b7f73fa451c5d119f86236a936ed28c72457e
Andrew Berger <andbberger@gmail.com>
2025-05-22 18:21:01 -0600

    updates stdout logging advisory

--- scripts/launch_bridge.bat
+++ scripts/launch_bridge.bat
@@ -1,6 +1,6 @@
 @echo off
 @echo watching for files
-@echo follow C:\Users\User\logs\bridge.log using mtail for info
+@echo follow C:\Users\User\logs\bridge.log using baretail for info
 @echo logs from previous days are renamed
 :: I resent this but there is some sort of environment running it elsewhere and I cannot be asked
 cd /d "C:\Users\User\src\brukerbridge"
